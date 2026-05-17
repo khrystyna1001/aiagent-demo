bf6f76b841769ebe04aff1f5bbd28f5637cf8fb2
Khrystyna <khrystyna.koshova@gmail.com>
2026-05-17 17:02:21 +0200

    completed chapter 3

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
@@ -23,3 +24,17 @@ def get_file_content(working_directory, file_path):
         return file_content_string
     except Exception as e:
         print(f"Error reading file {file_path}: {e}")
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Retrieves the content of a specified file relative to the working directory, with a maximum character limit to prevent excessive output",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file whose content is to be retrieved, relative to the working directory",
+            ),
+        },
+    ),
+)
--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from google.genai import types
 
 def run_python_file(working_directory: str, file_path: str, args=[]):
     abs_working_dir = os.path.abspath(working_directory)
@@ -35,3 +36,24 @@ def run_python_file(working_directory: str, file_path: str, args=[]):
     
     except Exception as e:
         return f"Error running {file_path}: {str(e)}"
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs a specified Python file relative to the working directory with optional command-line arguments, capturing and returning the standard output and error, while enforcing security constraints to prevent access outside the working directory",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file whose content is to be retrieved, relative to the working directory",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="An optional array of strings that represent command-line arguments to be passed to the Python file when executed",
+                items=types.Schema(
+                    type=types.Type.STRING
+                ),
+            ),
+        },
+    ),
+)
